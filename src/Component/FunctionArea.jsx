274a71654465135758756b3f9743dad644839775
chan-00 <a82316@naver.com>
2023-03-10 16:57:19 +0900

    function 영역 아이콘 버튼 추가

--- src/Component/FunctionArea.jsx
+++ src/Component/FunctionArea.jsx
@@ -1,6 +1,11 @@
+//import react hooks
+import { useEffect, useState } from "react";
+//import react styled component
 import styled from "styled-components";
+//import image src
 import userButtonSrc from "../Images/google-user-image.jpg";
 
+//전체 영역을 감싸는 컨테이너 css
 const AllContainerDiv = styled.div`
     border: 1px solid rgb(240, 240, 240);
     border-radius: 10px;
@@ -18,6 +23,7 @@ const AllContainerDiv = styled.div`
     padding-left: 10px;
 `;
 
+//각 아이콘 버튼들을 감싸는 div 영역에 대한 css
 const ButtonDiv = styled.div`
     width: 78px;
     height: 80px;
@@ -36,7 +42,6 @@ const ButtonDiv = styled.div`
     > img {
         width: 40px;
         height: 40px;
-        border-radius: 50%;
 
         margin: 10px 0 5px;
     }
@@ -50,91 +55,56 @@ const ButtonDiv = styled.div`
 `;
 
 function FunctionArea() {
+
+    //아이콘 이미지들의 src와 title 값을 저장할 useState 변수1
+    const [ iconInfoList1, setIconInfoList1 ] = useState([]);
+    //아이콘 이미지들의 src와 title 값을 저장할 useState 변수2
+    const [ iconInfoList2, setIconInfoList2 ] = useState([]);
+
+    //처음 렌더링될 때 아이콘들과 해당 버튼의 이름을 표시해주기 위한 useEffect 함수 코드
+    useEffect(() => {
+        const iconTempList1 = [];
+        const titleTempList1 = ["검색", "비즈니스 프로필", "지도", "YouTube", "Play", "뉴스", "Gmail", "Meet", "채팅", "주소록", "드라이브", "캘린더", "번역", "사진", "My Ad Center", "쇼핑"];
+        const iconTempList2 = [];
+        const titleTempList2 = ["금융", "문서", "스프레드시트", "프레젠테이션", "도서", "Blogger", "행아웃", "Keep", "Jamboard", "클래스룸", "어스", "컬렉션", "아트 앤 컬처", "Google Ads", "팟캐스트", "Google One", "여행", "설문지", "Google Store"];
+
+        for(let i = 1; i <= titleTempList1.length; i++) {
+            iconTempList1.push([require("../Images/google-function-image" + i + ".png"), titleTempList1[i - 1]]);
+        }
+        for(let i = 1; i <= titleTempList2.length; i++) {
+            iconTempList2.push([require("../Images/google-function-image" + (i + titleTempList1.length) + ".png"), titleTempList2[i - 1]]);
+        }
+
+        setIconInfoList1(iconTempList1);
+        setIconInfoList2(iconTempList2);
+    }, []);
+
+    console.log(iconInfoList1);
+    console.log(iconInfoList2);
+
     return (
         <AllContainerDiv>
             <ButtonDiv>
                 <img src={userButtonSrc} />
                 <p>Google 계정</p>
             </ButtonDiv>
+            {iconInfoList1.map((iconInfo, index) => (
+                <ButtonDiv key={index}>
+                    <img src={iconInfo[0]} />
+                    <p>{iconInfo[1]}</p>
+                </ButtonDiv>
+            ))}
+            <hr/>
+            {iconInfoList2.map((iconInfo, index) => (
+                <ButtonDiv key={index + 16}>
+                    <img src={iconInfo[0]} />
+                    <p>{iconInfo[1]}</p>
+                </ButtonDiv>
+            ))}
+            <hr/>
             <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>검색</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
-            </ButtonDiv>
-            <ButtonDiv>
-                <img src={userButtonSrc} />
-                <p>Google 계정</p>
+                <img src={require("../Images/google-function-image36.png")} />
+                <p>Cloud Drive<sup>TM</sup> to Mail</p>
             </ButtonDiv>
 
         </AllContainerDiv>
--- src/Svg/MicIcon.jsx
+++ src/Svg/MicIcon.jsx
@@ -11,7 +11,7 @@ const MicSvg = styled.svg`
 
 function MicIcon({title}) {
     return (
-        <MicSvg class="goxjub" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
+        <MicSvg className="goxjub" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" >
             <path fill="#4285f4" d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"></path>
             <path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path>
             <path fill="#fbbc05" d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"></path>
