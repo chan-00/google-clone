8f84c13f4158a64bcb0b7e5c62ca0fffcfe53520
chan-00 <a82316@naver.com>
2023-03-15 20:45:34 +0900

    profile area 헤더 버튼 영역 제작

--- src/Component/ProfileArea.jsx
+++ src/Component/ProfileArea.jsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 //import recoil
 import { useRecoilValue } from "recoil";
 import atomDisplayProfileArea from "../Atoms/atomDisplayProfileArea";
+//import image src
+import GoogleAccoundLogoSrc from "../Images/google-profile-image01.png";
+//import react bootstrap icon
+import { X, ThreeDotsVertical } from "react-bootstrap-icons";
 
 //카메라 버튼 클릭 시 나타나는 모달 창에 대한 전체 컨테이너 css
 const ProfileAllContainer = styled.div`
@@ -32,6 +36,44 @@ const ProfileContainer = styled.div`
     border-radius: 10px;
 `;
 
+//모달 창의 Header 영역 컨테이너 css
+const ProfileHeaderContainer = styled.div`
+    width: 100%;
+
+    text-align: center;
+
+    * {
+        cursor: pointer;
+    }
+
+    > svg:hover {
+        background-color: rgb(240, 240, 240);
+    }
+
+    > img {
+        width: 130px;
+        height: 27px;
+        cursor: auto;
+        margin-top: 13px;
+    }
+`;
+
+//X 버튼에 대한 CSS
+const XButton = styled(X)`
+    float: left;
+    font-size: 30px;
+    padding: 10px;
+    border-radius: 50%;
+`;
+
+//ThreeDotsVertical 버튼에 대한 CSS
+const ThreeDotsVerticalButton = styled(ThreeDotsVertical)`
+    float: right;
+    font-size: 20px;
+    padding: 15px;
+    border-radius: 50%;
+`;
+
 function ProfileArea() {
     //해당 영역의 display 값을 담고 있는 recoil 변수
     const profileArea = useRecoilValue(atomDisplayProfileArea);
@@ -39,7 +81,11 @@ function ProfileArea() {
     return (
         <ProfileAllContainer display={profileArea}>
             <ProfileContainer>
-
+                <ProfileHeaderContainer>
+                    <XButton />
+                    <img src={GoogleAccoundLogoSrc} />
+                    <ThreeDotsVerticalButton />
+                </ProfileHeaderContainer>
             </ProfileContainer>
         </ProfileAllContainer>
     )
