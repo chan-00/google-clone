1e0644f298b58a324aa6dcea37980f530be4661e
chan-00 <a82316@naver.com>
2023-03-16 12:06:20 +0900

    유저 프로필 사진 모달창 디자인 추가

--- src/Component/ProfileArea.jsx
+++ src/Component/ProfileArea.jsx
@@ -5,15 +5,16 @@ import { useRecoilValue } from "recoil";
 import atomDisplayProfileArea from "../Atoms/atomDisplayProfileArea";
 //import image src
 import GoogleAccoundLogoSrc from "../Images/google-profile-image01.png";
+import userButtonSrc from "../Images/google-user-image.jpg";
 //import react bootstrap icon
-import { X, ThreeDotsVertical } from "react-bootstrap-icons";
+import { X, ThreeDotsVertical, GlobeAmericas, ChevronRight, Pencil, Trash } from "react-bootstrap-icons";
 
 //카메라 버튼 클릭 시 나타나는 모달 창에 대한 전체 컨테이너 css
 const ProfileAllContainer = styled.div`
     position: fixed;
     width: 100%;
     height: 100%;
-    background-color: rgba(0,0,0,0.3);
+    background-color: rgba(0,0,0,0.5);
     transition: all 0.3s;
 
     display: ${props => props.display};
@@ -24,7 +25,7 @@ const ProfileAllContainer = styled.div`
 //카메라 버튼 클릭 시 나타나는 모달 창 컨테이너 css
 const ProfileContainer = styled.div`
     width: 400px;
-    height: 490px;
+    height: 485px;
 
     background-color: white;
 
@@ -51,7 +52,7 @@ const ProfileHeaderContainer = styled.div`
     }
 
     > img {
-        width: 130px;
+        width: 120px;
         height: 27px;
         cursor: auto;
         margin-top: 13px;
@@ -74,19 +75,139 @@ const ThreeDotsVerticalButton = styled(ThreeDotsVertical)`
     border-radius: 50%;
 `;
 
+//profile area의 컨텐츠 영역 컨테이너 css
+const ProfileContentsContainer = styled.div`
+    margin: 10px 25px;
+
+    > h3 {
+        font-weight: 400;
+        margin: 0;
+    }
+
+    > p {
+        font-size: 14px;
+        color: gray;
+    }
+`;
+
+//profile area의 컨텐츠 영역 중 모두에게 표시 영역에 대한 css
+const MarkButtonContainer = styled.div`
+    border-top: 1px solid rgb(180, 180, 180);
+    border-bottom: 1px solid rgb(180, 180, 180);
+
+    cursor: pointer;
+
+    padding: 17px 5px;
+    margin: 25px 0;
+
+    :hover {
+        background-color: rgb(250, 250, 250);
+    }
+
+    * {
+        color: rgb(80, 80, 80);
+        font-size: 14px;
+    }
+
+    > svg:first-child {
+        margin-left: 5px;
+        margin-right: 15px;
+    }
+
+    > span {
+        vertical-align: top;
+    }
+
+    > svg:last-child {
+        float: right;
+        margin-top: 3px;
+        font-size: 15px;
+    }
+`;
+
+//유저 프로필사진 영역을 감싸고 있는 컨테이너 css
+const UserProfileButtonContainer = styled.div`
+    width: 100%;
+    text-align: center;
+`;
+
+//유저 프로필사진 버튼에 대한 css
+const ButtonPersonCircle = styled.img.attrs({
+    src: `${userButtonSrc}`
+  })`
+    width: 145px;
+    height: 145px;
+    border-radius: 50%;
+    cursor: pointer;
+
+    margin-top: 10px;
+`;
+
+//변경&삭제 버튼 컨테이너에 대한 css
+const ModifyButtonContainer = styled.div`
+  width: 100%;
+  margin-top: 30px;
+
+  > button {
+    cursor: pointer;
+    width: 48%;
+    background-color: white;
+    color: blue;
+    border: 1px solid rgb(200, 200, 200);
+    padding: 7px 0;
+    border-radius: 5px;
+  }
+
+  > button:hover {
+    background-color: rgb(245, 255, 255);
+  }
+
+   svg {
+    margin-right: 10px;
+    vertical-align: bottom;
+  }
+
+  > button:first-child {
+    margin-right: 10px;
+  }
+`;
+
 function ProfileArea() {
     //해당 영역의 display 값을 담고 있는 recoil 변수
     const profileArea = useRecoilValue(atomDisplayProfileArea);
 
+
     return (
         <ProfileAllContainer display={profileArea}>
+
             <ProfileContainer>
                 <ProfileHeaderContainer>
                     <XButton />
                     <img src={GoogleAccoundLogoSrc} />
                     <ThreeDotsVerticalButton />
                 </ProfileHeaderContainer>
+
+                <ProfileContentsContainer>
+                    <h3>프로필 사진</h3>
+                    <p>사진을 추가하면 다른 사람이 나를 알아보기 쉬워지며 내가 계정에 로그인되어 있는지 확인할 수 있습니다.</p>
+
+                    <MarkButtonContainer>
+                        <GlobeAmericas/>
+                        <span>모두에게 표시</span>
+                        <ChevronRight/>
+                    </MarkButtonContainer>
+
+                    <UserProfileButtonContainer>
+                        <ButtonPersonCircle/>
+                    </UserProfileButtonContainer>
+
+                    <ModifyButtonContainer>
+                        <button><Pencil/>변경</button>
+                        <button><Trash/>삭제</button>
+                    </ModifyButtonContainer>
+                </ProfileContentsContainer>
             </ProfileContainer>
+
         </ProfileAllContainer>
     )
 }
