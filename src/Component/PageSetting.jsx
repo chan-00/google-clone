da42341607d7fe5b9ecab3c1be37af89600b4763
chan-00 <a82316@naver.com>
2023-03-22 20:28:28 +0900

    Chrome 맞춤설정 modal 창의 바로가기 클릭 영역 제작

--- src/Component/PageSetting.jsx
+++ src/Component/PageSetting.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components";
 //import react bootstrap icon
 import { Image, PaletteFill, Link } from "react-bootstrap-icons";
 //import react hooks
-import { useState } from "react";
+import { useEffect, useState } from "react";
+//import image src
+import MyShortCutsClicked from "../Images/google-page-setting-shortcuts01.PNG";
+import MyShortCuts from "../Images/google-page-setting-shortcuts02.PNG";
+import SiteShortCuts from "../Images/google-page-setting-shortcuts03.PNG";
+import SiteShortCutsClicked from "../Images/google-page-setting-shortcuts04.PNG";
 
 
 //Chrome 맞춤 설정 버튼 클릭 시 나타나는 모달 창에 대한 전체 컨테이너 css
@@ -44,7 +49,7 @@ const MainContentsAllContainer = styled.div`
     height: 87%;
 
     display: grid;
-    grid-template-columns: 30% 1fr;
+    grid-template-columns: 26% 1fr;
 `;
 
 //메인 컨텐츠의 버튼 영역을 감싸는 컨테이너 css
@@ -58,7 +63,7 @@ const MainContentsButtonContainer = styled.div`
 
 //메인 컨텐츠의 버튼 중 클릭되지 않은 버튼에 대한 css
 const MainContentsButton = styled.button`
-    width: 83%;
+    width: 90%;
     border-radius: 0 60px 60px 0;
     text-align: left;
 
@@ -97,7 +102,62 @@ const MainContentsClickedButton = styled(MainContentsButton)`
 
 //메인 컨텐츠의 헤더 영역을 감싸는 컨테이너 css
 const MainContentsHeaderContainer = styled.p`
-    margin: 40px 0;
+    margin: 40px 0 20px;
+`;
+
+//메인 컨텐츠의 내용 영역을 감싸는 컨테이너 css
+const MainContentsDetailContainer = styled.div`
+    width: 100%;
+    height: 80%;
+
+    
+`;
+
+//메인 컨텐츠 배경 내용의 각 영역을 감싸는 Card css
+const MainContentsBackgroundCard = styled.div`
+    border: 1px solid black;
+`;
+
+//메인 컨텐츠 중 바로가기 내용의 클릭 버튼 영역 컨테이너 css
+const MainContentsShortCutsClickContainer = styled.div`
+    width: 100%;
+    height: 75%;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    border: 1px solid black;
+`;
+
+//바로가기 내용에서 클릭 이미지 영역 css
+const MainContentsShortCutsClickImage = styled.div`
+    background: url(${props => props.imageUrl}) no-repeat center;
+    background-size: cover;
+    display: inline-block;
+
+    width: 275px;
+    height: 180px;
+
+    cursor: pointer;
+
+    margin: 0 5px;
+`;
+
+//바로가기 내용에서 클릭 이미지 아래 설명 텍스트 영역 컨테이너 css
+const MainContentsShortCutsClickTextContainer = styled.div`
+    width: 275px;
+    margin: 0 5px;
+
+    display: inline-block;
+
+    > p {
+        font-size: 13px;
+        margin: 0 5px 7px;
+    }
+
+    > p:first-child {
+        font-weight: bold;
+    }
 `;
 
 //버튼 영역을 감싸는 컨테이너 css
@@ -141,6 +201,15 @@ function PageSetting({ display, setDisplay }) {
     //현재 어떤 버튼을 클릭했는지에 대한 값이 담겨져 있는 useState 변수
     //1 : 배경, 2 : 바로가기, 3 : 색상 및 테마
     const [ clickButton, setClickButton ] = useState(1);
+    
+    //바로가기 메뉴에서 어떤 이미지를 클릭했는지에 대한 값이 담겨져 있는 useState 변수
+    //1 : 내 바로가기, 2 : 자주 방문한 사이트
+    const [ clickShortCutsImage, setClickShortCutsImage ] = useState(1);
+
+    //modal 창이 껐다 켜졌을 때 이전 메뉴가 뭐든 배경 메뉴가 클릭되어 있도록 하는 useEffect 함수
+    useEffect(() => {
+        setClickButton(1);
+    }, [display]);
 
     //버튼 클릭 시 해당 모달 창이 사라지게 하기 위한 이벤트 함수
     const handleClickButton = (e) => {
@@ -149,16 +218,25 @@ function PageSetting({ display, setDisplay }) {
     }
 
     //배경 버튼 클릭 시 호출되는 이벤트 함수
-    const handleClick1 = () => {
+    const handleClickBackground = () => {
         setClickButton(1);
     }
     //바로가기 버튼 클릭 시 호출되는 이벤트 함수
-    const handleClick2 = () => {
+    const handleClickShortCuts = () => {
         setClickButton(2);
     }
     //색상 및 테마 버튼 클릭 시 호출되는 이벤트 함수
-    const handleClick3 = () => {
+    const handleClickTheme = () => {
         setClickButton(3);
+    }
+
+    //바로가기 영역에서 내 바로가기 이미지 클릭 시 호출되는 이벤트 함수
+    const handleClickMyShortCutsImage = () => {
+        setClickShortCutsImage(1);
+    }
+    //바로가기 영역에서 내 바로가기 이미지 클릭 시 호출되는 이벤트 함수
+    const handleClickSiteShortCutsImage = () => {
+        setClickShortCutsImage(2);
     }
 
     return (
@@ -167,21 +245,48 @@ function PageSetting({ display, setDisplay }) {
                 <MainContentsAllContainer>
                     <MainContentsButtonContainer>
                         {clickButton === 1 ?
-                        <MainContentsClickedButton onClick={handleClick1}><Image />배경</MainContentsClickedButton> :
-                        <MainContentsButton onClick={handleClick1}><Image />배경</MainContentsButton>}
+                        <MainContentsClickedButton onClick={handleClickBackground}><Image />배경</MainContentsClickedButton> :
+                        <MainContentsButton onClick={handleClickBackground}><Image />배경</MainContentsButton>}
 
                         {clickButton === 2 ?
-                        <MainContentsClickedButton onClick={handleClick2}><Link />바로가기</MainContentsClickedButton> :
-                        <MainContentsButton onClick={handleClick2}><Link />바로가기</MainContentsButton>}
+                        <MainContentsClickedButton onClick={handleClickShortCuts}><Link />바로가기</MainContentsClickedButton> :
+                        <MainContentsButton onClick={handleClickShortCuts}><Link />바로가기</MainContentsButton>}
 
                         {clickButton === 3 ?
-                        <MainContentsClickedButton onClick={handleClick3}><PaletteFill />색상 및 테마</MainContentsClickedButton> :
-                        <MainContentsButton onClick={handleClick3}><PaletteFill />색상 및 테마</MainContentsButton>}
+                        <MainContentsClickedButton onClick={handleClickTheme}><PaletteFill />색상 및 테마</MainContentsClickedButton> :
+                        <MainContentsButton onClick={handleClickTheme}><PaletteFill />색상 및 테마</MainContentsButton>}
                     </MainContentsButtonContainer>
 
                     <div>
                         <MainContentsHeaderContainer>페이지 맞춤설정</MainContentsHeaderContainer>
-                        {clickButton === 1 ? <p>배경 내용입니당~</p> : clickButton === 2 ? <p>바로가기 내용입니당~</p> : <p>색상 및 테마 내용입니당~</p>}
+                        {clickButton === 1 ? 
+                        <MainContentsDetailContainer>
+                            배경 내용입니당~
+                        </MainContentsDetailContainer> 
+                        : clickButton === 2 ?
+                        <MainContentsDetailContainer>
+                            <MainContentsShortCutsClickContainer>
+                                {clickShortCutsImage === 1 ? 
+                                <MainContentsShortCutsClickImage imageUrl={MyShortCutsClicked} />
+                                : <MainContentsShortCutsClickImage imageUrl={MyShortCuts} onClick={handleClickMyShortCutsImage} /> }
+                                {clickShortCutsImage === 2 ? 
+                                <MainContentsShortCutsClickImage imageUrl={SiteShortCutsClicked} />
+                                : <MainContentsShortCutsClickImage imageUrl={SiteShortCuts} onClick={handleClickSiteShortCutsImage} /> }
+                                <MainContentsShortCutsClickTextContainer>
+                                    <p>내 바로가기</p>
+                                    <p>내가 선별한 바로가기입니다.</p>
+                                </MainContentsShortCutsClickTextContainer>
+                                <MainContentsShortCutsClickTextContainer>
+                                    <p>자주 방문한 사이트</p>
+                                    <p>자주 방문하는 웹사이트를 기반으로 추천된 바로가기입니다.</p>
+                                </MainContentsShortCutsClickTextContainer>
+                            </MainContentsShortCutsClickContainer>
+                        </MainContentsDetailContainer> 
+                        : 
+                        <MainContentsDetailContainer>
+                            색상 및 테마 내용입니당~
+                        </MainContentsDetailContainer>
+                        }
                     </div>
                 </MainContentsAllContainer>
 
